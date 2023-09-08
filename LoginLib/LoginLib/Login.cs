7ab045db84dbb6dcb077a9d94045195a4349e6d2
harsha1901@yahoo.com <harsha1901@yahoo.com>
2023-09-08 17:59:04 +0530

    Assignment

---
+++ LoginLib/LoginLib/Login.cs
@@ -0,0 +1,32 @@
+﻿namespace LoginLib
+{
+    public class Login
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public string LoginMethod()
+        {
+            if (string.IsNullOrWhiteSpace(Username) && string.IsNullOrWhiteSpace(Password))
+            {
+                throw new ArgumentException("Both username and password are empty or null.");
+            }
+            else if (string.IsNullOrWhiteSpace(Username))
+            {
+                throw new ArgumentException("Username is empty or null.");
+            }
+            else if (string.IsNullOrWhiteSpace(Password))
+            {
+                throw new ArgumentException("Password is empty or null.");
+            }
+            else if (Username == "Sam" && Password == "sam@123")
+            {
+                return "Login Successful";
+            }
+            else
+            {
+                return "Login Failed";
+            }
+        }
+    }
+}
---
+++ LoginLib/SpecFlowProject1/StepDefinitions/LoginStepDefinitions.cs
@@ -0,0 +1,77 @@
+using LoginLib;
+
+namespace SpecFlowProject1.StepDefinitions
+{
+    [Binding]
+    public sealed class LoginStepDefinitions
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+        private readonly Login _login;
+        private string _result;
+        private Exception _exception;
+
+        public LoginStepDefinitions(Login login)
+        {
+            _login = login;
+        }
+
+        [Given("the username is \"(.*)\"")]
+        public void GivenTheUserNameIs(string username)
+        {
+            try
+            {
+                _login.Username = username == "null" ? null : username;
+            }
+            catch (Exception ex)
+            {
+                _exception = ex;
+            }
+
+        }
+
+        [Given("the password is \"(.*)\"")]
+        public void GivenThePasswordIs(string password)
+        {
+            try
+            {
+                _login.Password = password == "null" ? null : password;
+            }
+            catch (Exception ex)
+            {
+                _exception = ex;
+            }
+
+        }
+
+        [When("I try to login")]
+        public void WhenITryToLogin()
+        {
+            try
+            {
+                _result = _login.LoginMethod();
+            }
+            catch (Exception ex)
+            {
+                _exception = ex;
+            }
+        }
+
+        [Then("the message should be \"(.*)\"")]
+        public void ThenTheMessageShouldBe(string result)
+        {
+            try
+            {
+                _result.Should().Be(result);
+            }
+            catch (Exception ex)
+            {
+                _exception = ex;
+            }
+        }
+        [Then("an exception should be thrown")]
+        public void ThenAnExceptionShouldBeThrown()
+        {
+            _exception.Should().NotBeNull();
+        }
+    }
+}
